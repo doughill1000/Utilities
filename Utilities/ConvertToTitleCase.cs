d4622609d8f12f8adb735ca157b7335f85e96902
dhill <doughill1000@gmail.com>
2015-03-09 14:45:53 -0400

    Commenting

--- Utilities/ConvertToTitleCase.cs
+++ Utilities/ConvertToTitleCase.cs
@@ -9,12 +9,17 @@ namespace Utilities
 {
     public class ConvertToTitleCase
     {
-        public static string ConvertToTitleCase(this string input)
+        /// <summary>
+        /// Takes a string input and converts it to TitleCase. Extension method of string class
+        /// </summary>
+        /// <param name="input">String parameter</param>
+        /// <returns>Formatted string</returns>
+        public static string ConvertToTitleCase(this string input) //
         {
             CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
             TextInfo textInfo = cultureInfo.TextInfo;
 
-            String output = input.ToLower();
+            String output = input.ToLower(); //String cannot be converted if all uppercase, therefore ToLower is used
             return textInfo.ToTitleCase(output);
         }
     }
