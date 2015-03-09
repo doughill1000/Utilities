e34e2b7d83827bc70e0e17a67b646a10ee88dfb1
dhill <doughill1000@gmail.com>
2015-03-09 14:44:18 -0400

    ConvertToTitleCase

---
+++ Utilities/ConvertToTitleCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace Utilities
+{
+    public class ConvertToTitleCase
+    {
+        public static string ConvertToTitleCase(this string input)
+        {
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+            TextInfo textInfo = cultureInfo.TextInfo;
+
+            String output = input.ToLower();
+            return textInfo.ToTitleCase(output);
+        }
+    }
+}
