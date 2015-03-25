9473af0b0da9183a6deabb0c75771a900d02bb7b
Doug Hill <doug.hill@airdye.com>
2015-03-25 13:27:47 -0400

    Added Json Download and Deserialize Functions

---
+++ Utilities/GetJsonObjectFromURL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Utilities
+{
+    public class JSON
+    {
+        public static string GetJsonObjectFromURL(string URL)
+        {
+            var json = new WebClient().DownloadString(URL);
+            return json;
+        }
+
+        public static List<object> DeserializeJsonObject(String json)
+        {
+            var jsonObject = JsonConvert.DeserializeObject<List<object>>(json);
+            return jsonObject;
+        }
+    }
+}
